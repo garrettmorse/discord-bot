2edb213ac3e5e1cc2bfa62dcf2b5c09fb0fe9bb7
garrettmorse <morsegarrett609@gmail.com>
2022-02-03 23:13:20 -0500

    feat: added basic infrastructure

---
+++ deployCommands.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const {
+  clientId, guildId, token, testGuildId,
+} = require('./config/constants');
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('score')
+    .setDescription('Displays Social Credit Score.')
+    .addUserOption((option) => option.setName('member')
+      .setDescription("Displays this Member's Social Credit Score.")
+      .setRequired(false)),
+]
+  .map((command) => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, testGuildId), { body: commands })
+  .then(() => console.log('Successfully registered application commands.'))
+  .catch(console.error);
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello');
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config/constants');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+  console.log('ready');
+});
+
+client.login(token);
+
+client.on('interactionCreate', console.log);
