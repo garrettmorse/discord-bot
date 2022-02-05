8ce2e88aca33ee584a8500e2ce073b53a382a22d
garrettmorse <morsegarrett609@gmail.com>
2022-02-04 20:37:22 -0500

    wip: tracking score

--- index.js
+++ index.js
@@ -1,12 +1,61 @@
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config/constants');
+const fs = require('fs');
+const {
+  token, testChannelId, testGuildId, guildId, clientId,
+} = require('./config/constants');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const bot = new Client({ partials: ['CHANNEL', 'MESSAGE'], intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
+let users = {};
 
-client.once('ready', () => {
+bot.once('ready', async (_bot) => {
   console.log('ready');
+  const userIds = [];
+  const guild = _bot.guilds.cache.get(guildId);
+  const members = await guild.members.list({ limit: 1000 });
+  members.forEach((_, v) => userIds.push(v));
+  users = userIds.reduce((acc, id) => ({ ...acc, [id]: 0 }), {});
+  fs.writeFileSync('./data/users.json', JSON.stringify(users, null, 2));
 });
 
-client.login(token);
+bot.login(token);
 
-client.on('interactionCreate', console.log);
+bot.on('messageReactionAdd', async (reaction, _user) => {
+  console.log(reaction);
+  switch (reaction.emoji.name) {
+    case '👍':
+      break;
+
+    case '👎':
+      break;
+
+    case 'ToadOk':
+      users[reaction.message.author.id] += 1;
+      break;
+
+    case 'NotOkToad':
+      users[reaction.message.author.id] -= 1;
+      break;
+
+    default:
+      break;
+  }
+});
+
+bot.on('messageReactionRemove', async (reaction, _user) => {
+  // 👎
+});
+
+bot.on('interactionCreate', (interaction) => {
+  const { user } = interaction;
+  const target = interaction.options.get('member');
+  switch (interaction.commandName) {
+    case 'score':
+      // if (interaction.channel.name !== 'bot-commands') return;
+      if (target && target.value === clientId) interaction.reply({ content: 'CCP have no score!' });
+      else if (target) interaction.reply({ content: `${target.user.username} has a score of ${users[target.value]}` });
+      else interaction.reply({ content: `${user.username} has a score of ${users[user.id]}` });
+      break;
+    default:
+      break;
+  }
+});
--- readme.md
+++ readme.md
@@ -1,3 +1,20 @@
 # Garrett's Discord Bot
 
 This bot is only meant to be used with my Discord server. Replicating functionality is not guaranteed nor supported. If you have any questions, let me know. :)
+
+
+### Data
+
+#### Credit Score
+
+```
+ Schema: JSON values of
+ [guildId]: { [clientId]: [socialCreditScore] }
+```
+
+#### Messages
+```
+ Schema: JSON values of
+ [messageId]: { [userId] }
+ Once a user has reacted to a message, there's no going back.
+ ```
