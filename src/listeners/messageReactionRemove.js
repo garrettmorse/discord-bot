559d44c9892f8103c6e25bc8470f8371521fc8d3
garrettmorse <morsegarrett609@gmail.com>
2022-02-05 21:41:13 -0500

    fix: bot log values and an eslint rule

--- src/listeners/messageReactionRemove.js
+++ src/listeners/messageReactionRemove.js
@@ -5,11 +5,11 @@ module.exports = function handleMessageReactionRemove(reaction, _user, users) {
   }
   switch (reaction.emoji.name) {
     case 'ToadOk':
-      console.log(`${_user.username} (${_user.id}) removed a +1 emoji from ${reaction.message.author.username}'s (${reaction.message.author.id}) message for a total of ${users[reaction.message.author.id] - 1}`);
+      console.log(`${_user.username} (${_user.id}) removed a +50 emoji from ${reaction.message.author.username}'s (${reaction.message.author.id}) message for a total of ${users[reaction.message.author.id] - 50}`);
       return users[reaction.message.author.id] - 50;
 
     case 'NotOkToad':
-      console.log(`${_user.username} (${_user.id}) removed a -1 emoji from ${reaction.message.author.username}'s (${reaction.message.author.id}) message for a total of ${users[reaction.message.author.id] + 1}`);
+      console.log(`${_user.username} (${_user.id}) removed a -100 emoji from ${reaction.message.author.username}'s (${reaction.message.author.id}) message for a total of ${users[reaction.message.author.id] + 100}`);
       return users[reaction.message.author.id] + 100;
 
     default:
